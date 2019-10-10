6bf79de1d6f1a51cc9c6e0a3eeecacaae8302b3f
ayatmustafa <ayatmustafa199472@gmail.com>
2019-10-10 17:15:50 +0200

    updated task

--- index.php
+++ index.php
@@ -25,13 +25,13 @@
                 <form action="" method="POST">
                 <div class="form-group pb-2">
                 <label for="price">Price</label>
-                <input type="text"
+                <input type="number" required 
                     class="form-control" name="price" id="" aria-describedby="priceId" placeholder="plz enter price of product">
                 <small hidden id="priceId" class="form-text text-muted">some thing error in price</small>
                 </div>
                 <div class="form-group pb-2">
                 <label for="numberp">Number of products</label>
-                <input type="text"
+                <input type="number" required 
                     class="form-control" name="numberp" id="" aria-describedby="numberpId" placeholder="plz enter number of products ">
                 <small hidden id="numberpId" class="form-text text-muted">some thing error in number of products</small>
                 </div>
@@ -44,17 +44,19 @@
 
             <div class="col-md-6 pt-5 text-light text-center">
             <?php if(isset($_POST['price']) && isset( $_POST['numberp'])){ 
-                if (is_numeric($_POST['price'] )&& is_numeric($_POST['numberp'] ) ) { 
-                    if ($_POST['price']>=0 && $_POST['numberp']>=0){?>
-            <h2>Total Price Befor discount  <?php  echo  $_POST['price'] *  $_POST['numberp'] ;  ?> </h2>
-           <h2>Total Price aftar discount  <?php  if(($_POST['price'] *  $_POST['numberp'])<=1000){ 
-               echo  (($_POST['price'] *  $_POST['numberp'] ) -(($_POST['price'] *  $_POST['numberp'])*(10/100))); 
+                $price=$_POST['price'];
+                $numberp=$_POST['numberp'];
+                if (is_numeric($price)&& is_numeric($numberp ) ) { 
+                    if ($price>0 && $numberp>0){?>
+            <h2>Total Price Befor discount  <?php  echo  $price *  $numberp ;  ?> </h2>
+           <h2>Total Price aftar discount  <?php  if(($price*  $numberp)<=1000){ 
+               echo  (($price *  $numberp ) -(($price *  $numberp)*(10/100))); 
 
             }
-            elseif(($_POST['price'] *  $_POST['numberp'])>1000){
+            elseif(($price *  $numberp)>1000){
 
-                echo  (($_POST['price'] *  $_POST['numberp'] ) -(($_POST['price'] *  $_POST['numberp'])*(15/100))); 
-            }elseif(($_POST['price'] *  $_POST['numberp'])<0){
+                echo  (($price*  $numberp ) -(($_POST['price'] *  $numberp)*(15/100))); 
+            }elseif(($price *  $numberp)<0){
                 echo '<h4 class="text-info  text-center w-100 text-captlized ">plz enter the price and number of products
                   and it  should be more than 0</h4><hr>';
 
@@ -62,10 +64,10 @@
 ?>
  </h2>
                 <p>number of prodacts:
-                    <?php echo $_POST['numberp']; ?>
+                    <?php echo $numberp; ?>
                 </p>
                 <p>price for one is :
-                    <?php echo $_POST['price']; ?>
+                    <?php echo $price; ?>
                 </p>
             
             <?php } else{
